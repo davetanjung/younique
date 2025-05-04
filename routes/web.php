c1402dae6865ec9499113fc3a7d0b74f83e9cd59
amandamich <amandamdarwis@gmail.com>
2025-05-04 22:16:52 +0700

    login page
    ui login page without middleware, harus ketik /login buat cek

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::post('/', [HomeController::class, 'post'])->name('home.post');
 
+Route::get('/login',[AuthController::class, 'show'])->name('login.show');
+Route::post('/login_auth', [AuthController::class, 'login_auth'])->name('login.auth');
+Route::get('/login', [AuthController::class,'show'])->name('login.show')->middleware('guest');
