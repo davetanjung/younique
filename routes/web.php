2f60dba549eefd698197677ad7b103de3637223d
amandamich <amandamdarwis@gmail.com>
2025-05-05 11:35:41 +0700

    Register Page
    Cek pake /register

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,5 @@ Route::post('/', [HomeController::class, 'post'])->name('home.post');
 Route::get('/login',[AuthController::class, 'show'])->name('login.show');
 Route::post('/login_auth', [AuthController::class, 'login_auth'])->name('login.auth');
 Route::get('/login', [AuthController::class,'show'])->name('login.show')->middleware('guest');
+Route::get('/register', function () {
+        return view('home.register') ;});
